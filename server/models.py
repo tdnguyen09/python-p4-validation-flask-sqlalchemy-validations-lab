f3e9be82fa1b22dd5f6b3281382bc4b4184cd41b
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-06-29 22:53:33 +1000

    complete lab

--- server/models.py
+++ server/models.py
@@ -11,7 +11,22 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    @validates('name')
+    def validate_name(self,key, name):
+        if not name:
+            raise ValueError('name required')
+
+        elif Author.query.filter(Author.name == name).first():
+            raise ValueError ('Author name is alreay exits')
+
+        return name
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if phone_number and len(phone_number) != 10 or  not phone_number.isdigit():
+            raise ValueError('phone number must be in 10 digits')
+        
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,7 +42,32 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Post must have a title.")
+        clickbait_keywords = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(keyword in title for keyword in clickbait_keywords):
+            raise ValueError("Post title must contain one of the following: 'Won't Believe', 'Secret', 'Top [number]', 'Guess'")
+        return title
+
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError('post conttent is at least 250 characters long')
+        return content
+    
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError('Post summary is maximum 250 characters')
+        return summary
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if category and category not in ["Fiction", "Non-Fiction"]:
+            raise ValueError("Category must be Fiction or Non-Fiction")
+        return category
 
 
     def __repr__(self):
